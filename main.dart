5ea3266415b6ec11281c5adfee0d23af91954ae5
Klutch <codewithklutch@gmail.com>
2020-02-19 07:15:12 +0000

    Added comments

--- main.dart
+++ main.dart
@@ -1,11 +1,16 @@
+/// A BASIC COMMAND LINE CASESAR CIPHER WRITTEN IN DART  ///
+/// Klutch // Klutchdevelopment@gmail.com // 02/19/2020 ///
+/// Written for part of a "CODE WITH KLUTCH" tutorial  ///
+
 import 'dart:io';
 
 void main() {
   // Creates a map of the cipher
 	Map<String, String> cipher = caesarCipher();
-  // Ask user for input
-  print('Enter a message');
-  // Stores the input to a variable
+
+  // Ask user to input a message they want to cipher
+  print('Enter a message',);
+  // Stores the message input to the output variable
   String output = stdin.readLineSync();
   String changed = "";
   for (String character in output.split('')) {
@@ -16,13 +21,16 @@ void main() {
     }
   }
   print(changed);
+  // Terminate the program properly using exit code '2' (Planned and successful exit)
   exit(2);
 }
 
 Map caesarCipher() {
   // Letters list
   const List<String> Letters = const ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  // Cipher Key value 
+
+  // Cipher Key value, which is hardcoated with a value of 13
+  // This allows the ciphered message to be ran through the program again to decipher
   int Key = 13;
   // Create a map of the converted characters
   Map<String, String> code = {};
@@ -36,8 +44,8 @@ Map caesarCipher() {
         code[Letters[i].toUpperCase()] = Letters[i - Key].toUpperCase();
       }
     }
-  // returns the cipher algorhythm
+  // return the ciphered message
   return code;
 }
-// Set exit code
+// Set the exit code
 void exitCode(int code) => exit(code);
